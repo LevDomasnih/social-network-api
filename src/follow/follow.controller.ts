443b54be1618515f5428bcf36696576aee4e5fdc
lev <lev.domasnix2013@gmail.com>
2021-10-21 23:34:56 +0500

    Добавил гвард на подписку

--- src/follow/follow.controller.ts
+++ src/follow/follow.controller.ts
@@ -1,16 +1,19 @@
-import { Body, Controller, Post, Headers } from '@nestjs/common';
+import { Body, Controller, Post, Headers, UseGuards } from '@nestjs/common';
 import { FollowService } from './follow.service';
 import { FollowDto } from './dto/follow.dto';
+import { JwtAuthGuard } from '../auth/guards/jwt.guard';
 
 @Controller('follow')
 export class FollowController {
     constructor(private readonly followService: FollowService) { }
 
+    @UseGuards(JwtAuthGuard)
     @Post('follow')
     async follow(@Body() dto: FollowDto) {
         return this.followService.follow(dto)
     }
 
+    @UseGuards(JwtAuthGuard)
     @Post('unfollow')
     async unfollow(@Headers() headers: any, @Body() dto: FollowDto) {
         return this.followService.unfollow(dto)
