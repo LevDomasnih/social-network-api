2bbd756678e2054db0e3afdfb427e3f4f3e4a3cd
lev <lev.domasnix2013@gmail.com>
2021-10-22 00:07:49 +0500

    Глобальная валидация

--- src/follow/follow.controller.ts
+++ src/follow/follow.controller.ts
@@ -1,21 +1,22 @@
-import { Body, Controller, Headers, UseGuards, Put, Delete } from '@nestjs/common';
+import { Body, Controller, Delete, Headers, Put, UseGuards } from '@nestjs/common';
 import { FollowService } from './follow.service';
 import { FollowDto } from './dto/follow.dto';
 import { JwtAuthGuard } from '../auth/guards/jwt.guard';
 
 @Controller('follow')
 export class FollowController {
-    constructor(private readonly followService: FollowService) { }
+    constructor(private readonly followService: FollowService) {
+    }
 
     @UseGuards(JwtAuthGuard)
     @Put()
     async follow(@Body() dto: FollowDto) {
-        return this.followService.follow(dto)
+        return this.followService.follow(dto);
     }
 
     @UseGuards(JwtAuthGuard)
     @Delete()
     async unfollow(@Headers() headers: any, @Body() dto: FollowDto) {
-        return this.followService.unfollow(dto)
+        return this.followService.unfollow(dto);
     }
 }
--- src/profile/profile.controller.ts
+++ src/profile/profile.controller.ts
@@ -1,20 +1,19 @@
-import { Body, Controller, Get, Param, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { ProfileService } from './profile.service';
 import { UpdateProfileDto } from './dto/update-profile.dto';
 
 @Controller('profile')
 export class ProfileController {
-    constructor(private readonly profileService: ProfileService) { }
-
-    @UsePipes(new ValidationPipe())
-    @Post('update')
-    async update(@Body() dto: UpdateProfileDto) {
-        return this.profileService.updateProfile(dto)
+    constructor(private readonly profileService: ProfileService) {
     }
 
-    @UsePipes(new ValidationPipe())
+    @Post('update')
+    async update(@Body() dto: UpdateProfileDto) {
+        return this.profileService.updateProfile(dto);
+    }
+
     @Get(':id')
     async get(@Param('id') id: string) {
-        return this.profileService.findProfile(id)
+        return this.profileService.findProfile(id);
     }
 }
