dba3b088c8c76a7e949b18ac504bb0c6733d0a2a
lev <lev.domasnix2013@gmail.com>
2021-11-01 23:47:19 +0500

    Начал настройку создания постов

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,9 +1,35 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { PostsService } from './posts.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { Express } from 'express';
+import { diskStorage } from 'multer';
 
 @Controller('posts')
 export class PostsController {
     constructor(
         private readonly postsService: PostsService
     ) { }
+
+    @Post('upload')
+    @UseInterceptors(FileInterceptor('file', {
+        storage: diskStorage({
+            destination: './files',
+
+        }),
+    }),)
+    // tslint:disable-next-line:no-any
+    async uploadFile(@UploadedFile() file: Express.Multer.File, @Body() body: any) {
+        console.log(file);
+        console.log(body.a);
+    }
+
+    @Post()
+    async createPost() {
+
+    }
+
+    @Get()
+    async getPost() {
+
+    }
 }
--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -2,19 +2,25 @@ import { Module } from '@nestjs/common';
 import { PostsController } from './posts.controller';
 import { PostsService } from './posts.service';
 import { TypegooseModule } from 'nestjs-typegoose';
+import { PostsModel } from './posts.model';
+import { MulterModule } from '@nestjs/platform-express';
 
 @Module({
-  controllers: [PostsController],
-  imports: [
-      TypegooseModule.forFeature([
-        {
-          typegooseClass: PostsService,
-          schemaOptions: {
-            collection: 'Posts'
-          }
-        }
-      ])
-  ],
-  providers: [PostsService]
+    controllers: [PostsController],
+    imports: [
+        TypegooseModule.forFeature([
+            {
+                typegooseClass: PostsModel,
+                schemaOptions: {
+                    collection: 'Posts',
+                },
+            },
+        ]),
+        MulterModule.register({
+            dest: './files',
+        }),
+    ],
+    providers: [PostsService],
 })
-export class PostsModule {}
+export class PostsModule {
+}
