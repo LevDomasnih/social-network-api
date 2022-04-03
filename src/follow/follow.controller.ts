a1e7676da40ba8ab2269360ff3c584ec1e075262
lev <lev.domasnix2013@gmail.com>
2022-04-04 01:03:47 +0500

    Обновил отписку

--- src/follow/follow.controller.ts
+++ src/follow/follow.controller.ts
@@ -1,11 +1,8 @@
-import { Body, Controller, Delete, Param, Put, UseGuards } from '@nestjs/common';
+import { Controller, Delete, Param, Put, UseGuards } from '@nestjs/common';
 import { FollowService } from './follow.service';
-import { FollowRequestDto } from './dto/follow-request.dto';
 import { JwtAuthGuard } from '../auth/guards/jwt.guard';
 import { ApiCreatedResponse, ApiTags } from '@nestjs/swagger';
-import { FollowResponseDto } from './dto/follow-response.dto';
 import { User } from '../decorators/user.decorator';
-import { UserModel } from '../users/user.model';
 import { UserEntity } from '../users/user.entity';
 import { IdValidationPipe } from '../pipes/id-validation.pipe';
 
@@ -19,7 +16,6 @@ export class FollowController {
     @UseGuards(JwtAuthGuard)
     @ApiCreatedResponse({
         description: 'Follow',
-        type: FollowResponseDto,
     })
     async follow(@Param('userId', IdValidationPipe) subscriberId: string, @User() user: UserEntity) {
         return this.followService.follow(user.id, subscriberId);
@@ -29,7 +25,6 @@ export class FollowController {
     @UseGuards(JwtAuthGuard)
     @ApiCreatedResponse({
         description: 'Unfollow',
-        type: FollowResponseDto,
     })
     async unfollow(@Param('userId', IdValidationPipe) subscriberId: string, @User() user: UserEntity) {
         return this.followService.unfollow(user.id, subscriberId);
--- src/follow/follow.service.ts
+++ src/follow/follow.service.ts
@@ -2,12 +2,10 @@ import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectModel } from 'nestjs-typegoose';
 import { ModelType } from '@typegoose/typegoose/lib/types';
 import { UserModel } from '../users/user.model';
-import { FollowRequestDto } from './dto/follow-request.dto';
 import { FollowModel } from './follow.model';
-import { Types } from 'mongoose';
 import { InjectRepository } from '@nestjs/typeorm';
 import { FollowEntity } from './follow.entity';
-import { createQueryBuilder, In, Repository } from 'typeorm';
+import { createQueryBuilder, Repository } from 'typeorm';
 import { UsersRepository } from '../users/users.repository';
 import { FindOneOptions } from 'typeorm/find-options/FindOneOptions';
 import { UserEntity } from '../users/user.entity';
@@ -21,7 +19,8 @@ export class FollowService {
         @InjectModel(FollowModel) private readonly followModel: ModelType<FollowModel>,
         @InjectRepository(FollowEntity) private readonly followRepository: Repository<FollowEntity>,
         @InjectRepository(SubscribersEntity) private readonly subscribersRepository: Repository<SubscribersEntity>,
-    ) { }
+    ) {
+    }
 
     async findUser(followUser: string, options?: FindOneOptions<UserEntity>) {
         return this.userRepository.findOne({ id: followUser }, options);
@@ -44,61 +43,54 @@ export class FollowService {
             .from(FollowEntity, 'follow')
             .where('follow.id = :followId', { followId: subscriber.id })
             .innerJoinAndSelect('follow.subscriber', 's')
-            .andWhere('s.subscriberOwnerId = :ownerId', {ownerId: subscriberOwner.id})
-            .execute();
+            .andWhere('s.subscriberOwnerId = :ownerId', { ownerId: subscriberOwner.id })
+            .getCount();
 
-        if (!!userIsFollow.length) {
+        if (!!userIsFollow) {
             throw new BadRequestException('Пользователь уже подписан');
         }
 
-        const follow1 = await this.followRepository.findOne(subscriber.id)
-        const follow2 = await this.followRepository.findOne(subscriberOwner.id)
+        const follow1 = await this.followRepository.findOne(subscriber.id);
+        const follow2 = await this.followRepository.findOne(subscriberOwner.id);
 
         const newFollow = await this.subscribersRepository.save({
             subscriber: follow1,
-            subscriberOwner: follow2
-        })
+            subscriberOwner: follow2,
+        });
 
-        if (!newFollow) { throw new BadRequestException('Не удалось подписаться') }
+        if (!newFollow) {
+            throw new BadRequestException('Не удалось подписаться');
+        }
 
-        return newFollow
+        return newFollow;
     }
 
     async unfollow(subscriberId: string, ownerId: string) {
-        // const followUser = await this.findUser(dto.followUserId);
-        // const user = await this.findUser(dto.userId);
-        //
-        // if (!followUser || !user) {
-        //     throw new BadRequestException('Данного пользователя не существует');
-        // }
-        //
-        // if (dto.followUserId === dto.userId) {
-        //     throw new BadRequestException('Пользователь не может отписаться от себя');
-        // }
-        //
-        // const followUserId = new Types.ObjectId(dto.followUserId)
+        const subscriberOwner = (await this.findUser(ownerId, { relations: ['follow'] }))?.follow;
+        const subscriber = (await this.findUser(subscriberId, { relations: ['follow'] }))?.follow;
 
-        // const userIsFollow = await this.followModel
-        //     .findOne({
-        //         _id: user.follow!._id,
-        //         followUser: { $in: [followUserId] }
-        //     })
-        //
-        // if (!userIsFollow) {
-        //     throw new BadRequestException('Пользователь не подписан');
-        // }
-        //
-        //
-        //
-        // const newFollow = await this.followModel
-        //     .findOneAndUpdate(
-        //         { _id: user.follow!._id },
-        //         { $pull: {followUser: followUserId} },
-        //         { new: true }
-        //     ).exec();
-        //
-        // if (!newFollow) { throw new BadRequestException('Не удалось отписаться') }
-        //
-        // return newFollow
+        if (!subscriberOwner || !subscriber) {
+            throw new BadRequestException('Данного пользователя не существует');
+        }
+
+        if (ownerId === subscriberId) {
+            throw new BadRequestException('Пользователь не может отписаться от себя');
+        }
+
+        const userFollow = await createQueryBuilder()
+            .select('s.id', 'id')
+            .from(FollowEntity, 'follow')
+            .where('follow.id = :followId', { followId: subscriber.id })
+            .innerJoinAndSelect('follow.subscriber', 's')
+            .andWhere('s.subscriberOwnerId = :ownerId', { ownerId: subscriberOwner.id })
+            .getRawOne();
+
+        if (!userFollow?.id) {
+            throw new BadRequestException('Пользователь не подписан');
+        }
+
+        const deleteSubscriber = await this.subscribersRepository.delete({ id: userFollow.id });
+
+        return deleteSubscriber.affected === 1; // TODO STATUS OK!!
     }
 }
